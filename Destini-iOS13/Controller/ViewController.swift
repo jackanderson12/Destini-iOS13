6f6843d2fddeac0c89ac7c3ac0c682466a398462
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2022-01-02 10:16:51 -0500

    Initial Commit
    Initial Commit

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    let story0 = Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right.")
+    var stories = [Story(title: "You see a fork in the road.", choice1: "Take a left.", choice2: "Take a right."), Story(title: "You see a Tiger.", choice1: "Shout for help.", choice2: "Play dead."), Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")]
+    var choice1Pressed: Bool
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        storyLabel.text = story0.title
+        choice1Button.setTitle(story0.choice1, for: .normal)
+        choice2Button.setTitle(story0.choice2, for: .normal)
 
     }
-
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        if sender.title(for: .normal) == choice1Button.title(for: .normal){
+            choice1Pressed = true
+        } else {
+            choice1Pressed = false
+        }
+        updateUI(choice1Pressed)
+    }
+    
+    func updateUI(_ choice1Pressed: Bool) {
+        if choice1Pressed {
+            
+        } else {
+            
+        }
+    }
 }
 
